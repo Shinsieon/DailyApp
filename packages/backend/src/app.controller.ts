fb1b86ae7165f69fbd001abdb32fd3913bc65b24
Shin Sie On <coolguysiun@gmail.com>
2025-01-19 00:04:57 +0900

    third

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Request, Res, UseGuards } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { join } from "path";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  serveFrontend(@Request() req, @Res() res) {
-    res.sendFile("index.html", { root: "../frontend/dist" });
+  @Get("*")
+  serveFrontend(@Res() res) {
+    const rootPath = join(__dirname, "../frontend/dist");
+    res.sendFile("index.html", { root: rootPath });
   }
 }
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,20 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { join } from "path";
+import * as express from "express";
 async function bootstrap() {
   let app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:8081',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: "http://localhost:8081",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
-
+  // 정적 파일 서빙
+  const frontendPath = join(__dirname, "../frontend/dist");
+  app.use(express.static(frontendPath));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(
     `Application is running on: ${await app.getUrl()}`,
-    'mode:',
-    process.env.NODE_ENV,
+    "mode:",
+    process.env.NODE_ENV
   );
 }
 
