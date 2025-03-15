fe407bca6cc401856a831809d6fec046ed90e80a
Shin Sie On <coolguysiun@gmail.com>
2025-03-15 14:04:24 +0900

    fix todoedit make alert 10min

--- packages/frontend/src/pages/TodoEditPage.tsx
+++ packages/frontend/src/pages/TodoEditPage.tsx
@@ -124,8 +124,24 @@ const timeColumns = [
       value: "00",
     },
     {
+      label: "10",
+      value: "10",
+    },
+    {
+      label: "20",
+      value: "20",
+    },
+    {
       label: "30",
       value: "30",
+    },
+    {
+      label: "40",
+      value: "40",
+    },
+    {
+      label: "50",
+      value: "50",
     },
   ],
 ];
